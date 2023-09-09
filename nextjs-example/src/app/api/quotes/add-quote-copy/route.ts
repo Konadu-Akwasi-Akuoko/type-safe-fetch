74e4ca8a7674401cc9e6d524e239856b8000f21f
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2023-09-09 12:17:03 +0000

    Created a function that can handle typesafe fetch api with zod

---
+++ nextjs-example/src/app/api/quotes/add-quote-copy/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+// This is also not type safe, how can we know the type of the incoming request?
+// We intend to response with the endpoint, method, body and data. Where body
+// is the body of the request if we received any, and data would be the body we
+// would return again minus any property that is not "id, text, author"
+export async function POST(req: Request) {
+  // const request = await req.json();
+  // console.log(request);
+  // const { text, author } = request;
+  return NextResponse.json({
+    endpoint: "quotes/add-quote",
+    method: req.method,
+    // body: request,
+    // data: { text, author },
+  });
+}
--- nextjs-example/src/ts-fetch/ts-fetch.ts
+++ nextjs-example/src/ts-fetch/ts-fetch.ts
@@ -1,33 +1,120 @@
-import { z } from "zod";
+import { ZodType, ZodTypeAny, z } from "zod";
 
 // Proposed fetch needs apiRequestSchema if there is a body, apiResponseSchema, url, methods, and body
+async function main() {
+  async function proposedFetch<T extends ZodTypeAny>({
+    url,
+    options,
+    responseSchema,
+  }: {
+    url: string;
+    options: Omit<RequestInit, "body">;
+    responseSchema: T;
+  }) {
+    // Create a function and return that function use an object
+    return {
+      query: async () => {
+        const request = await fetch(url, options);
+        const data: z.infer<T> = await request.json();
+        return data;
+      },
+      bodySchema: function <U extends ZodTypeAny>(bodySchema: U) {
+        return {
+          mutation: async (body: z.infer<U>) => {
+            const request = await fetch(url, {
+              ...options,
+              body: JSON.stringify(body),
+            });
+            const data: z.infer<T> = await request.json();
+            return data;
+          },
+        };
+      },
+    };
+  }
 
-async function proposedFetch(
-  url: string,
-  options: Omit<RequestInit, "body">,
-  requestBody: any
-) {
-  const apiRequestSchema = z.object({
-    name: z.string(),
-    age: z.number(),
+  // GET request
+  const tsFetch = await proposedFetch({
+    url: "http://localhost:3000/api/quotes",
+    options: { method: "GET", cache: "no-cache" },
+    responseSchema: z.object({
+      endpoint: z.string(),
+      method: z.string(),
+      body: z.null(),
+      data: z.object({
+        quotes: z.array(
+          z.object({ id: z.string(), text: z.string(), author: z.string() })
+        ),
+      }),
+    }),
   });
 
-  const apiResponseSchema = z.object({ name: z.string() });
+  const response = await tsFetch.query();
 
-  type responseType = z.infer<typeof apiResponseSchema>;
+  console.log(response.endpoint);
 
-  type bodyType = z.infer<typeof apiRequestSchema>;
-
-  const body: bodyType = { name: "Konadu", age: 23 };
-
-  const request = await fetch("https://example.com", {
-    method: "POST",
-    body: JSON.stringify(body),
+  // POST request with a body
+  const postFetch = await proposedFetch({
+    url: "http://localhost:3000/api/quotes/add-quote",
+    options: {
+      method: "POST",
+      cache: "no-cache",
+      headers: { "Content-Type": "application/json" },
+    },
+    responseSchema: z.object({
+      endpoint: z.string(),
+      method: z.string(),
+      body: z.any(),
+      data: z.object({ text: z.string(), author: z.string() }),
+    }),
   });
 
-  const response: responseType = await request.json();
+  const postResponse = await postFetch
+    .bodySchema(
+      z.object({
+        text: z.string(),
+        author: z.string(),
+      })
+    )
+    .mutation({ author: "Konadu", text: "Hello World" });
 
-  return response.name;
+  console.log(postResponse);
+
+  // POST without a body
+  const postFetchWithoutBody = await proposedFetch({
+    url: "http://localhost:3000/api/quotes/add-quote-copy",
+    options: {
+      method: "POST",
+      cache: "no-cache",
+      headers: { "Content-Type": "application/json" },
+    },
+    responseSchema: z.object({
+      endpoint: z.string(),
+      method: z.string(),
+      body: z.any(),
+      data: z.object({ text: z.string(), author: z.string() }),
+    }),
+  });
+
+  const postResponseWithoutBody = await postFetchWithoutBody
+    .bodySchema(z.null())
+    .mutation(null);
+
+  console.log(postResponseWithoutBody);
 }
 
-proposedFetch("https://example.com", { method: "POST" }, { name: "Konadu" });
+main();
+
+// Test function
+// function test(a: number) {
+//   console.log(a);
+//   return {
+//     data: (b: string) => {
+//       console.log(b);
+
+//       return b;
+//     },
+//   };
+// }
+
+// test(1).data("test");
